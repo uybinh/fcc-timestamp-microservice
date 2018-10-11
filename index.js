6d92d9f3ff2d9bb53790855b403fa2a21a51b3f8
Uy Binh <uybinh@gmail.com>
2018-10-11 20:35:59 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+app
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .listen(PORT, () => console.log(`App is listening on port ${PORT}`));
+
+app.get("/", (req, res) => res.render("pages/index"));
+
+app.get("/api/timestamp/:date_string?", (req, res) => {
+  const date = new Date(req.params.date_string);
+  const jsonData =
+    date.toString() === "Invalid Date"
+      ? { error: "Invalid Date" }
+      : { unix: +date, utc: date.toUTCString() };
+  res.json(jsonData);
+});
